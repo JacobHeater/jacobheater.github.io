718c29ce03609e76aabc44c2a8409b05f585ba25
Jacob Heater <jacobheater@gmail.com>
2025-04-30 17:22:17 -0400

    Attempting revitalization with next.js app

---
+++ src/app/about/resume/page.tsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Resume</h1>
+            <p className="text-sm text-gray-500">
+                This is a sample resume page.
+            </p>
+        </div>
+    );
+};
