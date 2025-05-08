9daf0c1a26f4d7c6e9af006625d4b0a799aabf47
Jacob Heater <jacobheater@gmail.com>
2025-05-08 01:31:29 -0400

    Update toolkit.ts

--- src/app/about/resume/data/technical-toolkit/toolkit.ts
+++ src/app/about/resume/data/technical-toolkit/toolkit.ts
@@ -10,4 +10,9 @@ AWS, Azure, Docker, Kubernetes, CI/CD
 
 **Tools & Practices**  
 Git, Agile (Scrum/Kanban), Visual Studio, VS Code, Linux/Windows
+
+**Management Philosophy**
+Trained in Manager Tools philosophy, weekly 1:1s, regular feedback,
+utilizing empirical data to measure results. Strong emphasis on
+servant leadership.
 `.trim();
