6ad2c820306c3afd33b25245f676a6bc3f5c5e00
Jacob Heater <jacobheater@gmail.com>
2025-05-08 23:45:15 -0400

    feat: Add generateStaticParams function for tag search page

--- src/app/blog/search/[tag]/page.tsx
+++ src/app/blog/search/[tag]/page.tsx
@@ -5,6 +5,10 @@ interface TagSearchProps {
   params: Promise<{ tag: string }>;
 }
 
+export function generateStaticParams() {
+  return [{tag: ''}];
+}
+
 export default async function TagSearch({ params }: TagSearchProps) {
   return <TagSearchClient tag={(await params).tag} />;
 }
