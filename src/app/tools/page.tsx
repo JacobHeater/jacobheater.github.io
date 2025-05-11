1a16478f8c6f7ccfe4288cafacfe788c50311d52
Jacob Heater <jacobheater@gmail.com>
2025-05-11 01:07:20 -0400

    fix: improve Markdown Viewer description for clarity and consistency

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -4,14 +4,27 @@ export default function ToolsPage() {
   return (
     <>
       <div className="text-center text-3xl font-bold my-10">Tools</div>
-      <div className="w-[90vw] md:w-[70vw] mx-auto">
+      <div className="w-[90vw] md:w-[70vw] mx-auto my-4">
         <p className="text-2xl font-bold my-8 text-[var(--accent)]">
           <Link href="/tools/markdown-viewer">Markdown Viewer</Link>
         </p>
         <p>
-          This tool allows you to view and print Markdown files. The viewer
-          supports LaTeX files for rendering math equations. No data is
-          transmitted to the server, and all processing is done in the browser.
+          View and print Markdown files effortlessly with this tool. It supports
+          LaTeX for rendering math equations, ensuring a seamless experience.
+          All processing is done locally in your browser, keeping your data
+          private and secure.
+        </p>
+      </div>
+      <div className="w-[90vw] md:w-[70vw] mx-auto my-4">
+        <p className="text-2xl font-bold my-8 text-[var(--accent)]">
+          <Link href="/tools/markdown-editor">Markdown Editor</Link>
+        </p>
+        <p>
+          This tool provides a powerful Markdown editing experience using the
+          Monaco Editor. It supports syntax highlighting, live preview, and
+          advanced features for creating and editing Markdown files. All
+          processing is done locally in your browser, ensuring privacy and
+          security.
         </p>
       </div>
     </>
