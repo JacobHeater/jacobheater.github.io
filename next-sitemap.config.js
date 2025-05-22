483fb81489ed735503c64da1432a59b96f974639
Jacob Heater <jacobheater@gmail.com>
2025-05-22 19:57:30 -0400

    feat: add sitemap and robots.txt for SEO optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://jacobheater.com',
+  generateRobotsTxt: true,
+  outDir: './public',
+  output: 'standalone'
+};
