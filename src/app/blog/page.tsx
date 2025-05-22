6f6deddb17d6e2351ebd847ebbbe19ed8a58f45d
Jacob Heater <jacobheater@gmail.com>
2025-05-22 19:49:23 -0400

    fix: restructure blog introduction for improved readability

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,11 +9,18 @@ export default function Blog() {
     <div className="flex flex-col">
       <HtmlTitle title="My Blog" />
       <div className="text-4xl font-bold py-5">My Blog</div>
-      <p className="my-8 text-lg">
-        Welcome to my blog! Here you'll find a range of topics from technology
-        to theology, to culinary adventures and coffee, and everything in
-        between. Thank you for reading!
-      </p>
+      <div className="my-8">
+        <p className="text-lg">
+          Welcome to my blog! Here you'll find a range of topics from technology
+          to theology, to culinary adventures and coffee, and everything in
+          between. Thank you for reading!
+        </p>
+        <p className="pt-4 italic">
+          The thoughts and opinions expressed here are my own and do not
+          necessarily reflect those of my employer or any affiliated
+          organizations.
+        </p>
+      </div>
       <div className="text-2xl font-bold">Blog Entries</div>
       <div className="mt-4 md:w-[60vw]">
         {blogEntryErd.getRootEntries().map((root) => renderTree(root, 0))}
